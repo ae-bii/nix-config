88e0ebea512f1a7cb7ff1991a45b738ac07c8d0c
ae-bii <anupambhakta1@gmail.com>
2024-04-15 21:47:03 -0400

    listed modules

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,7 +1,10 @@
 # Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
 {
-  # List your module files here
-  # my-module = import ./my-module.nix;
-  neovim = import ./neovim/default.nix;
+    # List your module files here
+    # my-module = import ./my-module.nix;
+    git = import ./git/default.nix;
+
+    neovim = import ./neovim/default.nix;
+    shell = import ./shell/default.nix;
 }
