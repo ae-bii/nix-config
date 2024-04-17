6947abb77cf08f3ce2c64bf4ba42e31055f91507
ae-bii <anupambhakta1@gmail.com>
2024-04-16 23:37:43 -0400

    added and formatted options

--- modules/home-manager/neovim/options.nix
+++ modules/home-manager/neovim/options.nix
@@ -3,12 +3,15 @@
 {
     programs.neovim = {
         extraLuaConfig = ''
-            vim.wo.number = true
+            vim.wo.number         = true
             vim.wo.relativenumber = true
-            vim.opt.expandtab = true
-            vim.opt.tabstop = 4
-            vim.opt.softtabstop = 4
-            vim.opt.shiftwidth = 4
+
+            vim.opt.expandtab     = true
+            vim.opt.tabstop       = 4
+            vim.opt.softtabstop   = 4
+            vim.opt.shiftwidth    = 4
+
+            vim.opt.showmode      = false
 	'';
     };
 }
