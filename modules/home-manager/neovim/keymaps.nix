06b550f992e13f491e0dea2cf7e4c873a14e5f69
ae-bii <anupambhakta1@gmail.com>
2024-04-16 23:08:54 -0400

    removed tab completion

--- modules/home-manager/neovim/keymaps.nix
+++ modules/home-manager/neovim/keymaps.nix
@@ -3,10 +3,7 @@
 {
     programs.neovim = {
         extraLuaConfig = ''
-	    vim.g.mapleader = "<Space>"
-
-            vim.keymap.set("i", "<C-n>", "<Tab>")
-            vim.keymap.set("i", "<C-y>", "<CR>")
+	        vim.g.mapleader = "<Space>"
 	'';
     };
 }
