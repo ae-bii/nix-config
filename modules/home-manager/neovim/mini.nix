ff4b02e751cdb48c1eccd1249dfdcaaf3f219688
ae-bii <anupambhakta1@gmail.com>
2024-04-16 23:32:38 -0400

    added new mini plugins

--- modules/home-manager/neovim/mini.nix
+++ modules/home-manager/neovim/mini.nix
@@ -6,7 +6,15 @@
             mini-nvim
         ];
         extraLuaConfig = ''
+            require('mini.align').setup()
+            require('mini.comment').setup()
             require('mini.completion').setup()
+            require('mini.cursorword').setup()
+            require('mini.pairs').setup()
+            require('mini.splitjoin').setup()
+            require('mini.statusline').setup()
+            require('mini.surround').setup()
+            require('mini.trailspace').setup()
         '';
     };
 }
