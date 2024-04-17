7d4bfaf649a6e9ff9982d7f2d2220307d2664b1b
ae-bii <anupambhakta1@gmail.com>
2024-04-16 22:58:52 -0400

    removed keymaps from options

--- modules/home-manager/neovim/options.nix
+++ modules/home-manager/neovim/options.nix
@@ -9,10 +9,6 @@
             vim.opt.tabstop = 4
             vim.opt.softtabstop = 4
             vim.opt.shiftwidth = 4
-
-            -- Remap for completion
-            inoremap <c-n> <tab>
-            inoremap <c-y> <cr>
 	'';
     };
 }
