a98ae36ff3de63f175e5e5ca854477c4470e69c9
ae-bii <anupambhakta1@gmail.com>
2024-04-18 12:25:46 -0400

    removed FIXME

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
+    # Add any other flake you might need
     hardware.url = "github:nixos/nixos-hardware";
 
     # Shameless plug: looking for a way to nixify your themes and make
@@ -59,7 +59,7 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      # FIXME replace with your hostname
+      # Replace with your hostname
       zero = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
@@ -72,7 +72,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
 #     homeConfigurations = {
-#       # FIXME replace with your username@hostname
+#       # Replace with your username@hostname
 #       "anu@zero" = home-manager.lib.homeManagerConfiguration {
 #         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
 #         extraSpecialArgs = {inherit inputs outputs;};
