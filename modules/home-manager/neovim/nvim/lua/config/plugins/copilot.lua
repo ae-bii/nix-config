ea1239b27d72fe5c843e236c3b11e061646bfab7
ae-bii <anupambhakta1@gmail.com>
2024-04-21 16:26:48 -0400

    added copilot

---
+++ modules/home-manager/neovim/nvim/lua/config/plugins/copilot.lua
@@ -0,0 +1,19 @@
+-- copilot
+-- https://github.com/github/copilot.vim
+
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		build = ":Copilot auth",
+		opts = {
+			-- suggestion = { enabled = false },
+			-- panel = { enabled = false },
+			-- filetypes = {
+			-- 	markdown = true,
+			-- 	help = true,
+			-- },
+		},
+	},
+}
