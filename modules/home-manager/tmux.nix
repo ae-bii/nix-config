939a82fd35c0a986e35b6526967f24fa27242eb9
ae-bii <anupambhakta1@gmail.com>
2024-04-21 14:42:44 -0400

    added tmux theme

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -3,7 +3,24 @@
 {
     programs.tmux = {
         enable = true;
+        terminal = "tmux-256color";
         clock24 = true;
         keyMode = "vi";
+        shortcut = "a";
+
+        plugins = with pkgs; [
+            {
+                plugin = tmuxPlugins.catppuccin;
+                extraConfig = '' 
+                    set -g @catppuccin_flavour 'mocha'
+                    set -g @catppuccin_window_tabs_enabled on
+                    set -g @catppuccin_date_time "%H:%M"
+                    '';
+            }
+        ];
+
+        extraConfig = ''
+            set -g default-terminal "tmux-256color"
+        '';
     };
 }
