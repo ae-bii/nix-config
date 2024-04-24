4095c209a527d27ca7786778d9e7e1210ae93d1f
Anupam Bhakta <anupambhakta1@gmail.com>
2024-04-23 23:50:58 -0400

    modified waybar theme

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -3,9 +3,11 @@
 killall waybar
 
 
-if [[ $USER = "anu" ]]
-then
-    waybar -c ~/nix-config/waybar/theme-config & -s ~/nix-config/waybar/style.css
-else
-    waybar &
-fi
+# if [[ $USER = "anu" ]]
+# then
+#     waybar -c ~/nix-config/waybar/theme-config & -s ~/nix-config/waybar/style.css
+# else
+#     waybar &
+# fi
+
+waybar -c ~/nix-config/waybar/theme-config & -s ~/nix-config/waybar/style.css
